05f148e930d9e5552cea750d379f70f6ba2daea8
sathishvasi <sathish@wizroots.com>
2020-10-02 16:28:59 +0530

    Unwanted content removed

--- js/main.js
+++ js/main.js
@@ -149,15 +149,15 @@
     /*---------------------------------
         Product Details Pic Slider
     ----------------------------------*/
-    $(".product__details__pic__slider").owlCarousel({
-        loop: true,
-        margin: 20,
-        items: 4,
-        dots: true,
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true
-    });
+    // $(".product__details__pic__slider").owlCarousel({
+    //     loop: true,
+    //     margin: 20,
+    //     items: 4,
+    //     dots: true,
+    //     smartSpeed: 1200,
+    //     autoHeight: false,
+    //     autoplay: true
+    // });
 
     /*-----------------------
 		Price Range Slider
@@ -188,16 +188,16 @@
     /*------------------
 		Single Product
 	--------------------*/
-    $('.product__details__pic__slider img').on('click', function () {
+    // $('.product__details__pic__slider img').on('click', function () {
 
-        var imgurl = $(this).data('imgbigurl');
-        var bigImg = $('.product__details__pic__item--large').attr('src');
-        if (imgurl != bigImg) {
-            $('.product__details__pic__item--large').attr({
-                src: imgurl
-            });
-        }
-    });
+    //     var imgurl = $(this).data('imgbigurl');
+    //     var bigImg = $('.product__details__pic__item--large').attr('src');
+    //     if (imgurl != bigImg) {
+    //         $('.product__details__pic__item--large').attr({
+    //             src: imgurl
+    //         });
+    //     }
+    // });
 
     /*-------------------
 		Quantity change
